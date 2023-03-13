f38d29fb2f540dfe94910a58684d5f3b71ed50c3
RashidIqbal-mri <rashidiqbal23705@gmail.com>
2023-03-14 01:25:22 +0530

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Rashid Iqbal. </span> As a web developer and designer, my objective is to combine my technical and creative skills to build visually stunning and highly functional websites that exceed client expectations. Design is not just what it looks like and feels like. Design is how it works."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
